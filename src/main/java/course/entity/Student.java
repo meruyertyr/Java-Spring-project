6e0e101d99ec734581938929b0bf47d9b855f367
meruyertyr <113831223+meruyertyr@users.noreply.github.com>
2024-09-20 13:07:39 +0600

    Update Student.java

--- src/main/java/course/entity/Student.java
+++ src/main/java/course/entity/Student.java
@@ -2,45 +2,50 @@ package course.entity;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
-import org.hibernate.validator.constraints.NotEmpty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Size(min = 3, message = "Firstname is invalid")
+    @Column(name = "firstname")
     private String firstName;
 
     @Size(min = 3, message = "Lastname is invalid")
+    @Column(name = "lastname")
     private String lastName;
 
     @Size(min = 2, message = "Department is invalid")
+    @Column(name = "department")
     private String department;
 
-    @NotEmpty
-    @Email
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(name = "email")
     private String email;
 
-    private Set<Course> courses = new HashSet<Course>(0);
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "student_course",
+        joinColumns = @JoinColumn(name = "student_id"),
+        inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Course> courses = new HashSet<>(0);
 
-    public Student() {
-    }
+    public Student() {}
 
     public Student(String firstName, String lastName, String department, String email) {
-        super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.department = department;
         this.email = email;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
@@ -49,7 +54,6 @@ public class Student {
         this.id = id;
     }
 
-    @Column(name = "firstname")
     public String getFirstName() {
         return firstName;
     }
@@ -58,7 +62,6 @@ public class Student {
         this.firstName = firstName;
     }
 
-    @Column(name = "lastname")
     public String getLastName() {
         return lastName;
     }
@@ -67,7 +70,6 @@ public class Student {
         this.lastName = lastName;
     }
 
-    @Column(name = "department")
     public String getDepartment() {
         return department;
     }
@@ -76,7 +78,6 @@ public class Student {
         this.department = department;
     }
 
-    @Column(name = "email")
     public String getEmail() {
         return email;
     }
@@ -85,11 +86,6 @@ public class Student {
         this.email = email;
     }
 
-    @ManyToMany(cascade = CascadeType.MERGE)
-    @JoinTable(name = "student_course", joinColumns = {
-        @JoinColumn(name = "id")}, inverseJoinColumns = {
-        @JoinColumn(name = "courseid")
-    })
     public Set<Course> getCourses() {
         return this.courses;
     }
@@ -99,11 +95,6 @@ public class Student {
     }
 
     public boolean hasCourse(Course course) {
-        for (Course studentCourse : getCourses()) {
-            if (studentCourse.getCourseid() == course.getCourseid()) {
-                return true;
-            }
-        }
-        return false;
+        return courses.stream().anyMatch(studentCourse -> studentCourse.getCourseid().equals(course.getCourseid()));
     }
 }
