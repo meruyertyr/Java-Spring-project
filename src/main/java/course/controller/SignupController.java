8c54c15e832e67901ca3dd52f87a5fffb3fbd8b1
meruyertyr <113831223+meruyertyr@users.noreply.github.com>
2024-09-20 13:09:48 +0600

    Update SignupController.java

--- src/main/java/course/controller/SignupController.java
+++ src/main/java/course/controller/SignupController.java
@@ -1,7 +1,6 @@
 package course.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import course.entity.User;
 import course.service.UserServiceImpl;
