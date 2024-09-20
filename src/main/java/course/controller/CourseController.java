10224dca4498c4448f8b2201995f18398dd3bb2d
meruyertyr <113831223+meruyertyr@users.noreply.github.com>
2024-09-20 13:08:54 +0600

    Update CourseController.java

--- src/main/java/course/controller/CourseController.java
+++ src/main/java/course/controller/CourseController.java
@@ -1,7 +1,7 @@
 package course.controller;
 
 import java.util.List;
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
